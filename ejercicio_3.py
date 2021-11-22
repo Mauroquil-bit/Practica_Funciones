862854aeab4847b09721ca167eb9abf9edea2eb3
Mauricio Mercado <75552002+Mauroquil-bit@users.noreply.github.com>
2021-11-22 09:56:31 -0300

    Ejercicio 3 actualizado

--- ejercicio_3.py
+++ ejercicio_3.py
@@ -3,6 +3,5 @@
 el cual se ha de pedir antes de llamar a la función.
 Esta función no devuelve ningún valor específico, 
 sino que devuelve el valor None. Definir la función con un parámetro (el nombre del usuario).
-
 """
 
